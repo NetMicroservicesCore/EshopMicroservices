9fcc26d95b8f6beaea49c19053efa95854c8273e
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2025-01-22 14:42:42 -0600

    se diseña la estructura base del proyecto con la implementacion de IRequestHandler y MediatR para trabajar con el patron de diseño CQRS

---
+++ src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.API.Models.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): IRequest<CreateProductResult>
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //en este apartado crearemos el producto.
+            throw new NotImplementedException();
+        }
+    }
+}
