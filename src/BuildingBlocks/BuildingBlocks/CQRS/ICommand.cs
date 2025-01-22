1462cd4f556223ef107cc3c752699a412eac0745
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2025-01-22 15:11:11 -0600

    se agregan los elementos base de la  arquitectura vertical utilizando un bloque generico para todos los posibles microservicios que integren nuestra solucion  de software.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+
+    /// <summary>
+    /// esta interfaz no es generica y no devuelve ningun tipo de valor
+    /// </summary>
+    public interface ICommand : ICommand<Unit>
+    {
+        
+    }
+    /// <summary>
+    /// Esta interfaz es generica y devuelve una respuesta generica
+    /// </summary>
+    /// <typeparam name="TResponse"></typeparam>
+    public interface ICommand<out TResponse>: IRequest<TResponse>
+    {
+    }
+}
