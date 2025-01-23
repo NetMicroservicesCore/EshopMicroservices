8a7ef31307fc066e91b5ffdd98f9cae716336444
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2025-01-23 16:59:36 -0600

    se corrije la compilacion ya que presento dos errores, con esta nueva correccion  tenemos la arquitectura base limpia para  trabajar con la implementacion de CQRS.

--- src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -3,7 +3,7 @@
 namespace Catalog.API.Models.Products.CreateProduct
 {
 
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): IRequest<CreateProductResult>
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): IRequest<CreateProductResult>;
     public record CreateProductResult(Guid Id);
     internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
