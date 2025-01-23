93ceb5c69a7c265c4303ef747f60679bafbe5f07
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2025-01-23 17:34:25 -0600

    creamos los primeros pasos de la implementacion del patron CQRS.

--- src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,15 +1,25 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
 
 namespace Catalog.API.Models.Products.CreateProduct
 {
 
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): IRequest<CreateProductResult>;
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            //en este apartado crearemos el producto.
+            //1.-en este apartado crearemos el producto desde el command object
+            Product producto = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+            //2.-guardamos en base de datos
+            //3.-regresamos  el CreateProductResult result
             throw new NotImplementedException();
         }
     }
