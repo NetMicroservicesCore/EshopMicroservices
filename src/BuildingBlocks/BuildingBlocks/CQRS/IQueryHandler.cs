d5948855979dc3e6f3b2d47492fe7f0271c76652
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2025-01-23 16:56:18 -0600

    se referencia  la libreria de BuildingBlocks  con el catalgo principal del producto.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+    /// <summary>
+    /// Esta interfaz gestiona las solicitudes de consulta y se encarga de que cada consulta corresponda a el tipo de
+    /// respuesta solicitado.
+    /// </summary>
+    /// <typeparam name="TQuery">la consulta generica</typeparam>
+    /// <typeparam name="TResponse">la respuesta generica</typeparam>
+    public interface IQueryHandler<in TQuery, TResponse>: IRequestHandler<TQuery,TResponse> where TQuery: IQuery<TResponse> where TResponse: notnull
+    {
+
+    }
+}
