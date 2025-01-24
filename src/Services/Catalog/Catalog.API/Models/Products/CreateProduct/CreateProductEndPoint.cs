75b20dd7ae96887cd53f76042398f7eacc2858b6
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2025-01-24 16:21:23 -0600

    Agregamos nuestra clase de uso global para los using de esta manera utilizamos buenas practicas en la importacion de los using dentro de la estrcutura principal de las clases.

--- src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,10 +1,8 @@
-﻿using Carter;
-using Mapster;
-using MediatR;
+﻿
 
 namespace Catalog.API.Models.Products.CreateProduct
 {
-    
+
     public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
     
     public record CreateProductResponse(Guid id);
