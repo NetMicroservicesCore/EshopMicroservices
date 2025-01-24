db820fb851e535f4a810045788d692f7d748283e
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2025-01-24 09:46:43 -0600

    se agrega carter como libreria base para el tratamiento del routing de las minimal api.

--- src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,10 +1,16 @@
-﻿namespace Catalog.API.Models.Products.CreateProduct
+﻿using Carter;
+
+namespace Catalog.API.Models.Products.CreateProduct
 {
     
     public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
     
     public record CreateProductResponse(Guid id);
-    public class CreateProductEndPoint
+    public class CreateProductEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
