fc8ae29412054f03cb85f13f1478222f1b0441e2
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2025-01-24 16:35:03 -0600

    se configura los servicios de AddCarter y MediaTr para la implementacion de las librerias necesarias para interactuar con las minimal apis.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+//registramos las librerias carter, mapster , mediaTr
+builder.Services.AddCarter();
+//registramos los servicios de MediaTr y configuramos sus servicios 
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
-
-
+///
+app.MapCarter();
 
 app.Run();
