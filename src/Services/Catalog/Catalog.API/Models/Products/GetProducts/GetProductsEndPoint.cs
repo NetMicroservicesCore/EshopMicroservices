fefa7b82540084eb0e5294d74b6b8cc3aa782244
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-12 16:52:42 -0600

    modificamos el inicio dela clase GetProductsEndPoint

--- src/Services/Catalog/Catalog.API/Models/Products/GetProducts/GetProductsEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/GetProducts/GetProductsEndPoint.cs
@@ -1,6 +1,15 @@
-﻿namespace Catalog.API.Models.Products.GetProducts
+﻿
+namespace Catalog.API.Models.Products.GetProducts
 {
-    public class GetProductsEndPoint
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) => {
+
+                var result = await sender.Send(new GetProductsQuery());
+            })
+        }
     }
 }
