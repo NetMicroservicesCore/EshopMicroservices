ea339771d5ad2387910a0489afc56e1c99096fcc
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-12 00:27:29 -0600

    se agregan los elementos record que se encargaran de devolver una estructura segun el caso de uso, especifico.

--- src/Services/Catalog/Catalog.API/Models/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/GetProducts/GetProductsHandler.cs
@@ -1,5 +1,14 @@
 ﻿namespace Catalog.API.Models.Products.GetProducts
 {
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+    /// <summary>
+    /// Devolvemos  una lista o ienumerable de productos
+    /// este record representa  la solicitud de obtencion de productos y la estructura de productos
+    /// que esperamos.
+    /// </summary>
+    /// <param name="Products"></param>
+    public record GetProductsResult(IEnumerable<Product> Products);
     public class GetProductsHandler
     {
     }
