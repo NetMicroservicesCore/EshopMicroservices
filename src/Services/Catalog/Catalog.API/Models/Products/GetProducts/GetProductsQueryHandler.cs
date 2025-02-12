1ba131177be642a3063f055b6cd7ffbbef2245be
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-12 00:48:49 -0600

    se genera la nueva estructura de el productsquery para trabajar con  el metodo handler

---
+++ src/Services/Catalog/Catalog.API/Models/Products/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Catalog.API.Models.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+    /// <summary>
+    /// Devolvemos  una lista o ienumerable de productos
+    /// este record representa  la solicitud de obtencion de productos y la estructura de productos
+    /// que esperamos.
+    /// </summary>
+    /// <param name="Products"></param>
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    /// <summary>
+    /// Este clase se encarga de gestionar la consulta de GetProducts
+    /// y se encarga de devolver al resultado de Getproduct
+    /// </summary>
+    internal class GetProductsQueryHandler
+        (IDocumentSession session, ILogger<GetProductsQueryHandler>logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Hanlde llamado con {@Query}", query);
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        }
+    }
+}
