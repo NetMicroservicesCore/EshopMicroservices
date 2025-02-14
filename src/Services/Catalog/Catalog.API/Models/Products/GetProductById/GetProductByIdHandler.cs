bf3150f44c2d8b0fb18f0a5cd5e5df60e8cce3f9
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-13 19:50:12 -0600

    se escriben clases de excepcion para controlar los posibles errores dentro de las peticiones

--- src/Services/Catalog/Catalog.API/Models/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,15 +1,24 @@
 ﻿
+using Catalog.API.Exceptions;
+
 namespace Catalog.API.Models.Products.GetProductById
 {
     public record GetProductByIdQuery(Guid Id):IQuery<GetProductByIdResult>;
 
     public record GetProductByIdResult(Product Product);
 
-    internal class GetProductByIdHandler : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    internal class GetProductByIdHandler(IDocumentSession session, ILogger<GetProductByIdHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
-        public Task<GetProductByIdResult> Handle(GetProductByIdQuery queryt, CancellationToken cancellationToken)
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("GetProductByIdQueryHandler.Handle se llamo con {@Query}", query);
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+            if (product is null) 
+            {
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(product);
         }
     }
 }
