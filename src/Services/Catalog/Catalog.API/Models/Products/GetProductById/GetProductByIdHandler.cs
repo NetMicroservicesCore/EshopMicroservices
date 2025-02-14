31caf285b542be884d91da6e1c6b2631618aad6f
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-13 19:28:12 -0600

    se crea la estructura base de GetProductByHandler

--- src/Services/Catalog/Catalog.API/Models/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,7 +1,15 @@
-﻿namespace Catalog.API.Models.Products.GetProductById
+﻿
+namespace Catalog.API.Models.Products.GetProductById
 {
     public record GetProductByIdQuery(Guid Id):IQuery<GetProductByIdResult>;
-    public class GetProductByIdHandler
+
+    public record GetProductByIdResult(Product Product);
+
+    internal class GetProductByIdHandler : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
+        public Task<GetProductByIdResult> Handle(GetProductByIdQuery queryt, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
