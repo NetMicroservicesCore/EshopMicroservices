bf1bb33c9923b3f1ccfadb158b5a5543524e8249
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-15 12:12:51 -0600

    se arega la clase completa GetProductByCategoryEndPoint con todos sus metodos necesarios

--- src/Services/Catalog/Catalog.API/Models/Products/GetProductByCategory/GetProductByCategoryEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/GetProductByCategory/GetProductByCategoryEndPoint.cs
@@ -1,7 +1,23 @@
-﻿namespace Catalog.API.Models.Products.GetProductByCategory
+﻿
+namespace Catalog.API.Models.Products.GetProductByCategory
 {
-
-    public class GetProductByCategoryEndPoint
+    //public record GetProductByCategoryRequest()
+    public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+    public class GetProductByCategoryEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{ category}", async (string category, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByCategoryQuery(category));
+                var response = result.Adapt<GetProductByCategoryResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetProductByCategory")
+                .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Obtener Producto por categoria")
+                .WithDescription("Obtener Producto por Categoria");
+        }
     }
 }
