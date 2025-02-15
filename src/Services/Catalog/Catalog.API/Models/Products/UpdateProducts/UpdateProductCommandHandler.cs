8c3ba4731ca2ab9a88bb34828197ee6ad759f112
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-15 14:27:43 -0600

    se termina el metodo manejador de la actualizacion del producto.

--- src/Services/Catalog/Catalog.API/Models/Products/UpdateProducts/UpdateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/UpdateProducts/UpdateProductCommandHandler.cs
@@ -8,11 +8,24 @@ namespace Catalog.API.Models.Products.UpdateProducts
     /// <summary>
     /// Esta clase se encargara de establecer el orden de actualizacion del producto.
     /// </summary>
-    internal class UpdateProductCommandHandler : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
-        public Task<UpdateProductResult> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogInformation("UpdateProeuctHandler.Handle llamado con {@Command}", command);
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+            product.Name = command.Name;
+            product.Category = command.Category;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new UpdateProductResult(true);
         }
     }
 }
