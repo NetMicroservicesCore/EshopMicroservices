b626a1d46723641bef1a4d8778ee18f47578d57b
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-15 14:02:54 -0600

    se agregan los comandos del tipo record para establecer el flujo del trabjo del manejador de la actualizacion del producto.

---
+++ src/Services/Catalog/Catalog.API/Models/Products/UpdateProducts/UpdateProductHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.API.Models.Products.UpdateProducts
+{
+    public record UpdateProductCommand(Guid Id,string Name, List<string> Category, string Description, string ImageFile,decimal Price):
+        ICommand<UpdateProductResult>;
+
+        public record UpdateProductResult(bool IsSuccess);
+    /// <summary>
+    /// Esta clase se encargara de establecer el orden de actualizacion del producto.
+    /// </summary>
+    public class UpdateProductHandler
+    {
+    }
+}
