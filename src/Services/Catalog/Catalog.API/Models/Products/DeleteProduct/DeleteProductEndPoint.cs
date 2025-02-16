6a11fd554460929d75b2137b05a762a0a51d63ca
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-15 21:31:44 -0600

    se termina todo el proceso de eliminacionde proeuctos por identificador.

--- src/Services/Catalog/Catalog.API/Models/Products/DeleteProduct/DeleteProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Models/Products/DeleteProduct/DeleteProductEndPoint.cs
@@ -1,6 +1,25 @@
-﻿namespace Catalog.API.Models.Products.DeleteProduct
+﻿
+namespace Catalog.API.Models.Products.DeleteProduct
 {
-    public class DeleteProductEndPoint
+    //public record DeleteProductRequest(Guid Id)
+
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+                var response = result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Borrar Producto")
+            .WithDescription("Eliminar Producto");
+        }
     }
 }
