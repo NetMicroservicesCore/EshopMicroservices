1dfd06784400f18f430794573feffe8cc2eee877
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-02-15 20:37:43 -0600

    se carga la primera clase para el caso de uso deleteProductCommand

---
+++ src/Services/Catalog/Catalog.API/Models/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Catalog.API.Models.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id):ICommand<DeleteProductResult>;
+
+    public record DeleteProductResult(bool IsSuccess);
+    internal class DeleteProductCommandHandler : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
